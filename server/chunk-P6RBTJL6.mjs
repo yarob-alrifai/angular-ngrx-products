88da0650ee42d36610fe9224ca48bcda0b449878
yarob <yalrifai41@gmail.com>
2024-05-19 12:30:12 +0300

    Auto-generated commit

---
+++ server/chunk-P6RBTJL6.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{L as a}from"./chunk-TUZBJYHQ.mjs";import"./chunk-WABBGMOC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as AdminModule};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-5M4PI3YZ.mjs";import"./chunk-DIDRTW3Y.mjs";import"./chunk-TUZBJYHQ.mjs";import"./chunk-WABBGMOC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
